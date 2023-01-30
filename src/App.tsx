9caf014147b162b8e597f28f19a3ca21a2857e77
elodieEdohAlove <elodie.edohalove@yahoo.fr>
2023-01-30 15:10:14 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  ChakraProvider,
+  refineTheme,
+  ReadyPage,
+  ErrorComponent,
+  Layout
+} from "@pankod/refine-chakra-ui";
+
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { ChakraUIInferencer } from "@pankod/refine-inferencer/chakra-ui";
+
+function App() {
+  return (
+    <ChakraProvider theme={refineTheme}>
+      <Refine
+        dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+        notificationProvider={notificationProvider()}
+        ReadyPage={ReadyPage}
+        catchAll={<ErrorComponent />}
+        Layout={Layout}
+        routerProvider={routerProvider}
+        resources={[
+          {
+            name: "products",
+            list: ChakraUIInferencer,
+            show: ChakraUIInferencer,
+            create: ChakraUIInferencer,
+            edit: ChakraUIInferencer
+          }
+        ]}
+      />
+    </ChakraProvider>
+  );
+}
+
+export default App;
